70861a57c3726a7a531fad2eaf6e37c2820e015f
dkim9108 <kda91@naver.com>
2021-04-05 11:51:22 +0900

    두번째 버전

--- web1.py
+++ web1.py
@@ -9,4 +9,15 @@ soup = BeautifulSoup(page, "html.parser")
 #print(soup.prettify())
 #<P>를 모두 검색. 리스트 형식으로 리턴
 #print( soup.find_all("p"))
-print( soup.find("p"))
+#print( soup.find("p"))
+#print(soup.find_all("p", class_="outer-text"))
+#특정 id속성검색
+#print(soup.find(id="first"))
+#<a>태그가져오기
+#print(soup.find_all("b"))
+
+#tag를 제거하고 내부 문자열(컨텐츠만)
+#<p>컨텐츠</p>
+for tag in soup.find_all("p"):
+    #앞뒤에 공백문자를 제거
+    print(tag.text.strip())
